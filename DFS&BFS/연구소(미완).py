c497745e3252edbed400e86cf448f3d7bf2ef29e
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-02-02 18:15:18 +0900

    Update 연구소(미완).py

--- DFS&BFS/연구소(미완).py
+++ DFS&BFS/연구소(미완).py
@@ -9,7 +9,6 @@ for i in range(n) :
 
 back_up = copy.deepcopy(graph)
 
-    
 visited = [[False] * m for i in range(n)]
 virus = []
 
@@ -50,19 +49,49 @@ def bfs(graph) :
             graph[nx][ny] = 2
             queue.append((nx,ny))
             
+def solution(graph) :
+    index = deque()
+    for i in range(n) :
+        for j in range(m) :
+            index.append((i,j))
+            
+    max_value = 0
     
-find_virus(graph)
-ans = 0
-for i in range(n) :
-    for j in range(m) :
+    v = index.popleft()
+
+    while index :
+        for i in range(len(index)) :
+            for j in range(i+1, len(index)) :
+                # 임의의 벽을 세울 자리에 이미 벽이 있거나 바이러스 자리일 경우 continue
+                if graph[v[0]][v[1]] == 1 or graph[v[0]][v[1]] == 2 or graph[index[j][0]][index[j][1]] == 1 or graph[index[i][0]][index[i][1]] == 1 or graph[index[j][0]][index[j][1]] == 2 or graph[index[i][0]][index[i][1]] == 2 :
+                    continue
+                print(v, index[i],index[j])
+                
+                # 벽을 세개 세운다
+                graph[v[0]][v[1]] = 1
+                graph[index[i][0]][index[i][1]] = 1
+                graph[index[j][0]][index[j][1]] = 1
+                
+                for row in range(m) :
+                    for col in range(n) :
+                        print(graph[row][col] , end = " ")
+                    print()
+                
+                print()                
+                
+                # bfs를 이용한 시뮬레이션을 한다.
+                bfs(graph)
+                # 안전지대의 최소값을 구해준다.
+                max_value = max(check(graph), max_value)
+                # 그래프를 원래 상태로 돌려 놓는다.
+                graph = back_up
+                
+        v = index.popleft()
+    
+    return max_value
         
-        graph[i][j] = 1
-        graph[i][j] = 1
-        graph[i][j] = 1
+find_virus(graph)
+print(solution(graph))
 
-
-
-for i in range(n) :
-    for j in range(m) :
-        print(graph[i][j], end = " ")
-    print()
+print(graph)
+print(graph)
