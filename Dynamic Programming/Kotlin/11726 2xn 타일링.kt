cc7dfa9d8ad89c0165b2f3b2bc0cba7983aa94c9
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-03-20 17:24:15 +0900

    Create 11726 2xn 타일링.kt

---
+++ Dynamic Programming/Kotlin/11726 2xn 타일링.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner
+import kotlin.math.*
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+    val num = sc.nextInt()
+    
+    var dp = LongArray(1001, {0})
+    
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    
+    for(i in 4..num) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    }
+    
+    println("${dp[num]}")
+
+}
