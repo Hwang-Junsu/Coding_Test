678273483b03834b7dbfd250569903d138012962
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-04-27 21:10:41 +0900

    Update 디스크 컨트롤러.py

--- Implementation/programmers/Lv.3/디스크 컨트롤러.py
+++ Implementation/programmers/Lv.3/디스크 컨트롤러.py
@@ -1,4 +1,5 @@
 import heapq
+import math
 
 # 소요시간이 짧은 순서부터 처리한다
 # 대기시간 + 소요시간 계산을 잘 해야한다
@@ -20,24 +21,37 @@ def solution(jobs):
     for job in jobs :
         renewal.append((job[1], job[0]))
     heapq.heapify(renewal)
-    
+    check = False
     save = []
     # 뒤집어서 만들어 준 새로운 배열을 하나씩 꺼내어 시간 계산 작업을 한다.
     while renewal :
-        time, start = heapq.heappop(renewal)
+        if not check : time, start = heapq.heappop(renewal)
         
-        if now < start : #다음 소요시간이 낮은 작업이 현재 시간보다 시작시간이 크다면 잠시 저장.
+        if now < start and not check : #다음 소요시간이 낮은 작업이 현재 시간보다 시작시간이 크다면 잠시 저장.
             save.append((time,start))
+            if len(renewal) == 0 :
+                check = True
+                
+                save.sort(key = lambda x : x[1])
+                
+                time, start = save.pop(0)
+                for t, s in save :
+                    heapq.heappush(renewal, (t,s))
+                save.clear()
             continue
-
-        workingTime.append((now - start) + time)
-        now = now + time
+        
+        if check :
+            workingTime.append(time)
+            now = time + start
+            check = False
+        else :
+            workingTime.append((now - start) + time)
+            now = now + time
         
         if len(save) > 0 :
             for t, s in save :
                 heapq.heappush(renewal, (t,s))
             save.clear()
-
     
-    answer = sum(workingTime) // len(workingTime)
+    answer = math.floor(sum(workingTime) / len(workingTime))
     return answer
