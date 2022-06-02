64bd928a3f59476db934106dcf40d9ed5dd9a5dc
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-06-02 17:04:46 +0900

    Create 9019 DSLR.cpp

---
+++ Class/Class <3>/9019 DSLR.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int D(int num) {
+    int n = num * 2;
+    if(n > 10000) n %= 10000;
+    return n;
+}
+
+int S(int num) {
+    int n = num-1;
+    if(n < 0) n = 9999;
+    return n;
+}
+
+int L(int num) {
+    
+    
+}
+
+int R(int num) {
+    
+    
+    
+}
+
+int main() {
+    int t; cin >> t;
+    
+    while(t-- > 0) {
+        
+        int a,b; cin >> a >> b;
+        
+        
+    }
+}
