2ce62d2f7c1d66542789224d8db60b1b412e3d43
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-06-03 17:46:46 +0900

    Update 10026 적록색약.cpp

--- Class/Class <3>/10026 적록색약.cpp
+++ Class/Class <3>/10026 적록색약.cpp
@@ -1,3 +1,5 @@
+// BFS, 구현 문제.
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,33 +7,40 @@ using namespace std;
 char board[101][101];
 int visited[101][101];
 int n, mxarea;
-int dx[8] = {-1,-1,0,1,1,1,0,-1};
-int dy[8] = {0,1,1,1,0,-1,-1,-1};
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
 
-void bfs(int x, int y) {
-    
-    visited[x][y] = 1;
+void bfs(int x, int y, int& c) {
+    if(visited[x][y] > 0) return;
+    visited[x][y] = c++;
+    mxarea = max(visited[x][y], mxarea);
     deque<pair<int, int>> queue;
     queue.push_back(make_pair(x,y));
     while(!queue.empty()) {
         
         auto v = queue.front(); queue.pop_front();
         
-        for(int i = 0 ; i < 8; i++) {
+        for(int i = 0 ; i < 4; i++) {
             
             int nx = dx[i] + v.first; int ny = dy[i] + v.second;
             if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
             if(visited[nx][ny] > 0) continue;
             
-            if(board[nx][ny] == board[v.first][v.second]) visited[nx][ny] = visited[v.first][v.second];
-            else visited[nx][ny] = visited[v.first][v.second] + 1;
-            mxarea = max(visited[nx][ny], mxarea);
-            
-            queue.push_back(make_pair(nx, ny));
+            if(board[nx][ny] == board[v.first][v.second]) {
+                visited[nx][ny] = visited[v.first][v.second];
+                queue.push_back(make_pair(nx, ny));
+            }
         }
     }
-
-
+}
+void change() {
+    
+    for(int i = 0 ; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(board[i][j] == 'G') board[i][j] = 'R';
+        }
+    }
+    
 }
 
 int main() {
@@ -41,13 +50,28 @@ int main() {
             cin >> board[i][j];
         }
     }
-    bfs(0,0);
-    
-    for(int i = 0 ; i < n; i++) {
-        for(int j = 0 ;j < n; j++) {
-            cout << visited[i][j] << " ";
+    int rgb = 0; int rb = 0;
+    int count = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0 ; j < n; j++) {
+            bfs(i,j, count);    
         }
-        cout << '\n';
     }
+    rgb = mxarea; mxarea = 0; count = 1;
+    for(int i = 0 ; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            visited[i][j] = 0;
+        }
+    }
+    change();
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0 ; j < n; j++) {
+            bfs(i,j, count);    
+        }
+    }
+    rb = mxarea;
+    
+    cout << rgb << " " << rb;
     
 }
