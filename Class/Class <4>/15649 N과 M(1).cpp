77ae18aed8e31e79814d21e9b4dcee252039182e
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-06-05 15:38:45 +0900

    Create 15649 N과 M(1).cpp

---
+++ Class/Class <4>/15649 N과 M(1).cpp
@@ -0,0 +1,35 @@
+// 백트레킹 기법 연습.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int n,m;
+bool visited[9];
+
+void dfs(int number, string str) {
+    
+    if(number > m) {
+        cout << str << '\n'; return;
+    }
+    
+    for(int i = 1 ; i <= n; i++) {
+        if(!visited[i]) {
+            
+            visited[i] = true;
+            dfs(number + 1, str + to_string(i) + " ");
+            visited[i] = false;
+        }
+    }
+    
+}
+
+
+int main() {
+    cin >> n >> m;
+    
+    
+    dfs(1, "");
+}
