c5ee35bc13e6b83add7cf7b179601993ceb4f90d
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-06-15 17:33:45 +0900

    Create 9084 동전.cpp

---
+++ Class/Class <4>/9084 동전.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int c[21];
+int v[21];
+int dp[21][10001];
+
+int main() {
+    int t; cin >> t;
+    while(t-- > 0) {
+        
+        int n; cin >> n;
+        for(int i = 1; i <= n; i++) {
+            cin >> c[i];
+        }
+        int p; cin >> p;
+        
+        
+        
+        
+        
+    }
+}
