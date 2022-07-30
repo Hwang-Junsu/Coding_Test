f4d4b9c7bedfc14c5776ac5675b5998b4751beda
Hwang-Junsu <80745897+Hwang-Junsu@users.noreply.github.com>
2022-07-30 16:52:37 +0900

    Update 2922 즐거운 단어.py

--- Daily/2922 즐거운 단어.py
+++ Daily/2922 즐거운 단어.py
@@ -34,3 +34,41 @@ def check(string, compare, index, max_length, c_count, v_count) :
 check(word, "" , 0 ,length ,0 , 0)
 print(len(arr))
 
+
+# 갯수만 구하기
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+c = ['A','E','I','O','U']
+v = [chr(ord('A')+i) for i in range(26) if chr(ord('A')+i) not in c]
+word = input()
+def check(string, index, c_count, v_count, contain) :
+    if c_count >= 3 : return 0
+    if v_count >= 3 : return 0
+    if len(string) == index :
+        if contain > 0 :
+            return 1
+        else :
+            return 0
+    answer = 0
+    if string[index] != '_' :
+        if string[index] in c :
+            answer = check(string,  index+1, c_count+1, 0, contain)
+        elif string[index] in v :
+            if string[index] == "L" :
+                answer = check(string, index+1, 0, v_count+1, contain+1)
+            else :
+                answer = check(string, index+1, 0, v_count+1, contain)
+    else :
+        answer += 5*check(string, index+1, c_count+1, 0, contain)
+        answer += 1*check(string, index+1, 0, v_count+1, contain+1)
+        answer += 20*check(string, index+1, 0, v_count+1, contain)
+
+    return answer
+
+
+print(check(word , 0 ,0 , 0, 0))
+
+
