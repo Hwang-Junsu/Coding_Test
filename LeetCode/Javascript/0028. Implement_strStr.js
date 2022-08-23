2605ed8cac91a3fb28ab34b9c89685395f81f141
Hwang-Junsu <hjs77728@gmail.com>
2022-08-23 11:11:00 +0900

    solve

---
+++ LeetCode/Javascript/0028. Implement_strStr.js
@@ -0,0 +1,35 @@
+/*
+
+Implement strStr().
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+ 
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+*/
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  const len = needle.length;
+
+  for (let i = 0; i < haystack.length; i++) {
+    let str = haystack.substr(i, len);
+    if (needle === str) {
+      return i;
+    }
+  }
+  return -1;
+};
