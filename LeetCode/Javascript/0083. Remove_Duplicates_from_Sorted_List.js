0aac53cac8442957fdb0ff9fb3232589e22cb461
Hwang-Junsu <hjs77728@gmail.com>
2022-08-25 22:40:07 +0900

    solve

---
+++ LeetCode/Javascript/0083. Remove_Duplicates_from_Sorted_List.js
@@ -0,0 +1,36 @@
+/*
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var deleteDuplicates = function (head) {
+  let newNode = new ListNode(-101);
+  let cur = newNode;
+
+  while (true) {
+    if (head === null) break;
+    if (cur.val !== head.val) {
+      cur.next = new ListNode(head.val);
+      cur = cur.next;
+    }
+    head = head.next;
+  }
+  return newNode.next;
+};
