0246d734dcc4189992e358a028a534910461faeb
Hwang-Junsu <hjs77728@gmail.com>
2022-11-24 22:45:37 +0900

    ing

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const arr = require("fs")
-    .readFileSync(__dirname + "/input.txt")
-    .toString()
-    .trim()
-    .split("\n")
-    .map((el) => el.trim());
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-const answer = [];
-for (let i = 0; i < arr.length - 1; i++) {
-    const number = arr[i];
-    if (number.length === 1) {
-        answer.push("yes");
-        continue;
+// N극은 0, S극은 1
+const gears = [
+  [...arr[0]].map((el) => +el),
+  [...arr[1]].map((el) => +el),
+  [...arr[2]].map((el) => +el),
+  [...arr[3]].map((el) => +el),
+];
+const k = Number(arr[4]);
+
+const clockwiseRotate = (arr) => {
+  arr.unshift(arr.pop());
+};
+
+const counterClockwiseRotate = (arr) => {
+  arr.push(arr.shift());
+};
+
+for (let i = 5; i < k + 5; i++) {
+  const [rotateGear, direction] = arr[i].split(" ").map((el) => +el);
+  const rotate = [0, 0, 0, 0];
+  const isSame = [
+    [false, false],
+    [false, false],
+    [false, false],
+    [false, false],
+  ];
+  rotate[rotateGear - 1] = direction;
+  for (let i = 0; i < 3; i++) {
+    if (gears[i][2] === gears[i + 1][6]) {
+      isSame[i][1] = true;
+      isSame[i + 1][0] = true;
     }
-    let isPalin = true;
-    for (let j = 0; j <= parseInt(number.length / 2); j++) {
-        if (number[j] !== number[number.length - 1 - j]) {
-            answer.push("no");
-            isPalin = false;
-            break;
-        }
-    }
-    if (isPalin) answer.push("yes");
+  }
+  console.log(isSame);
 }
-console.log(answer.join("\n"));
+
+// a톱니바퀴를 회전시켰을 때 옆에있는 톱니바퀴와 맞닿은 극이 다르다면 (a[2] !== b[6]);
+// b는 a와 회전한 방향 반대로 연쇄적으로 돌아간다.
+// 만약 맞닿은 극이 같다면 회전하지 않는다.
