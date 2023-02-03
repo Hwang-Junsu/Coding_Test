d8449df2f4c198e802ada1d90bf2912a7dc9565a
Hwang-Junsu <hjs77728@gmail.com>
2023-02-03 15:18:22 +0900

    solve

--- Beakjoon/Javascript/14891 톱니바퀴.js
+++ Beakjoon/Javascript/14891 톱니바퀴.js
@@ -1,91 +1,131 @@
 const arr = require("fs")
-    .readFileSync(__dirname + "/input.txt")
-    .toString()
-    .trim()
-    .split("\n");
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.trim());
 
-// N극은 0, S극은 1
-const gears = [
-    [...arr[0]].map((el) => +el),
-    [...arr[1]].map((el) => +el),
-    [...arr[2]].map((el) => +el),
-    [...arr[3]].map((el) => +el),
+const inputs = [
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
 ];
-const k = Number(arr[4]);
-let answer = 0;
+const k = +arr.shift();
+const rotate = arr.map((el) => el.split(" ").map((el) => +el));
 
-const clockwiseRotate = (arr) => {
-    arr.unshift(arr.pop());
-};
-
-const counterClockwiseRotate = (arr) => {
-    arr.push(arr.shift());
-};
-
-const visited = [false, false, false, false];
-
-for (let i = 5; i < k + 5; i++) {
-    const [rotateGear, direction] = arr[i].split(" ").map((el) => +el);
-    const gear = rotateGear - 1;
-    const rotate = [0, 0, 0, 0]; // 0은 가만히, 1은 시계, -1은 반시계
-    rotate[gear] = direction;
-    const rotateStack = [gear];
-
-    while (rotateStack.length !== 0) {
-        const current = rotateStack.pop();
-        if (visited[current]) continue;
-        visited[current] = true;
-
-        if (current === 0) {
-            if (gears[current][2] !== gears[current + 1][6]) {
-                rotate[current + 1] = rotate[current] === -1 ? 1 : -1;
-
-                if (!visited[current + 1]) rotateStack.push(current + 1);
-            }
-        }
-        if (current === 1) {
-            if (gears[current][6] !== gears[current - 1][2]) {
-                rotate[current - 1] = rotate[current] === -1 ? 1 : -1;
-                if (!visited[current - 1]) rotateStack.push(current - 1);
-            }
-            if (gears[current][2] !== gears[current + 1][6]) {
-                rotate[current + 1] = rotate[current] === -1 ? 1 : -1;
-                if (!visited[current + 1]) rotateStack.push(current + 1);
-            }
-        }
-        if (current === 2) {
-            if (gears[current][6] !== gears[current - 1][2]) {
-                rotate[current - 1] = rotate[current] === -1 ? 1 : -1;
-                if (!visited[current - 1]) rotateStack.push(current - 1);
-            }
-            if (gears[current][2] !== gears[current - 1][2]) {
-                rotate[current + 1] = rotate[current] === -1 ? 1 : -1;
-                if (!visited[current + 1]) rotateStack.push(current + 1);
-            }
-        }
-        if (current === 3) {
-            if (gears[current][6] !== gears[current - 1][2]) {
-                rotate[current - 1] = rotate[current] === -1 ? 1 : -1;
-                if (!visited[current - 1]) rotateStack.push(current - 1);
-            }
-        }
-    }
-
-    for (let i = 0; i < rotate.length; i++) {
-        if (rotate[i] === 1) clockwiseRotate(gears[i]);
-        if (rotate[i] === -1) counterClockwiseRotate(gears[i]);
-    }
+class Gear {
+  constructor(state) {
+    this.state = state;
+  }
+  rightValue() {
+    return this.state[2];
+  }
+  leftValue() {
+    return this.state[6];
+  }
+  topValue() {
+    return this.state[0];
+  }
+  clockDirectionRotate() {
+    this.state.unshift(this.state.pop());
+  }
+  counterClockDirectionRotate() {
+    this.state.push(this.state.shift());
+  }
 }
 
-for (let i = 0; i < 4; i++) {
-    if (i === 0 && gears[i][0] === 1) answer += 1;
-    if (i === 1 && gears[i][0] === 1) answer += 2;
-    if (i === 2 && gears[i][0] === 1) answer += 4;
-    if (i === 3 && gears[i][0] === 1) answer += 8;
+class Gears {
+  constructor(gears, order) {
+    this.gears = gears.map((gear) => new Gear(gear));
+    this.order = order;
+  }
+  orderStack = []; // 돌아가야하는 오더들의 목록 ([기어숫자, 방향])
+  workStack = [];
+  visited = [false, false, false, false];
+  point = 0;
+
+  work(gear, dir) {
+    // 방향에 따라 기어를 돌린다.
+    if (dir === 1) {
+      this.gears[gear].clockDirectionRotate();
+    } else {
+      this.gears[gear].counterClockDirectionRotate();
+    }
+  }
+
+  leftCheck(gear, dir) {
+    // 왼쪽 기어를 확인한다.
+    if (gear - 1 < 0) return;
+    if (this.visited[gear - 1]) return;
+    if (this.gears[gear - 1].rightValue() !== this.gears[gear].leftValue()) {
+      dir === -1
+        ? this.orderStack.push([gear - 1, 1])
+        : this.orderStack.push([gear - 1, -1]);
+    }
+  }
+  rightCheck(gear, dir) {
+    // 오른쪽 기어를 확인한다.
+    if (gear + 1 > 3) return;
+    if (this.visited[gear + 1]) return;
+    if (this.gears[gear].rightValue() !== this.gears[gear + 1].leftValue()) {
+      dir === -1
+        ? this.orderStack.push([gear + 1, 1])
+        : this.orderStack.push([gear + 1, -1]);
+    }
+  }
+
+  check(start, dir) {
+    this.orderStack.push([start, dir]);
+    while (this.orderStack.length > 0) {
+      const [gear, dir] = this.orderStack.shift();
+      this.visited[gear] = true;
+      this.leftCheck(gear, dir);
+      this.rightCheck(gear, dir);
+
+      this.workStack.push([gear, dir]);
+    }
+  }
+
+  do() {
+    while (this.workStack.length > 0) {
+      const [gear, dir] = this.workStack.shift();
+      this.work(gear, dir);
+    }
+    this.visited = [false, false, false, false];
+  }
+
+  countPoint() {
+    for (let i = 0; i < this.gears.length; i++) {
+      if (this.gears[i].topValue() === 1) {
+        this.point += 2 ** i;
+      }
+    }
+  }
+
+  print() {
+    console.log(this.gears);
+  }
 }
 
-console.log(answer);
+const gears = new Gears(inputs);
 
-// a톱니바퀴를 회전시켰을 때 옆에있는 톱니바퀴와 맞닿은 극이 다르다면 (a[2] !== b[6]);
-// b는 a와 회전한 방향 반대로 연쇄적으로 돌아간다.
-// 만약 맞닿은 극이 같다면 회전하지 않는다.
+for (let i = 0; i < rotate.length; i++) {
+  gears.check(rotate[i][0] - 1, rotate[i][1]);
+  gears.do();
+}
+gears.countPoint();
+
+console.log(gears.point);
--- index.js
+++ index.js
@@ -1,88 +1,131 @@
-const input = require("fs")
+const arr = require("fs")
   .readFileSync(__dirname + "/input.txt")
   .toString()
   .trim()
   .split("\n")
   .map((el) => el.trim());
 
-const [n, m, y, x, k] = input
-  .shift()
-  .split(" ")
-  .map((el) => +el);
+const inputs = [
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+  arr
+    .shift()
+    .split("")
+    .map((el) => +el),
+];
+const k = +arr.shift();
+const rotate = arr.map((el) => el.split(" ").map((el) => +el));
 
-const orders = input
-  .pop()
-  .split(" ")
-  .map((el) => +el);
-
-const map = input.map((el) => el.split(" ").map((el) => +el));
-
-class Dice {
-  constructor(front, back, left, right, ceil, ground) {
-    this.front = front;
-    this.back = back;
-    this.left = left;
-    this.right = right;
-    this.ground = ground;
-    this.ceil = ceil;
+class Gear {
+  constructor(state) {
+    this.state = state;
   }
+  rightValue() {
+    return this.state[2];
+  }
+  leftValue() {
+    return this.state[6];
+  }
+  topValue() {
+    return this.state[0];
+  }
+  clockDirectionRotate() {
+    this.state.unshift(this.state.pop());
+  }
+  counterClockDirectionRotate() {
+    this.state.push(this.state.shift());
+  }
+}
 
-  rollDice(dir) {
-    let temp = 0;
-    temp = this.ceil;
+class Gears {
+  constructor(gears, order) {
+    this.gears = gears.map((gear) => new Gear(gear));
+    this.order = order;
+  }
+  orderStack = []; // 돌아가야하는 오더들의 목록 ([기어숫자, 방향])
+  workStack = [];
+  visited = [false, false, false, false];
+  point = 0;
+
+  work(gear, dir) {
+    // 방향에 따라 기어를 돌린다.
     if (dir === 1) {
-      this.ceil = this.left;
-      this.left = this.ground;
-      this.ground = this.right;
-      this.right = temp;
-    }
-    if (dir === 2) {
-      this.ceil = this.right;
-      this.right = this.ground;
-      this.ground = this.left;
-      this.left = temp;
-    }
-    if (dir === 3) {
-      this.ceil = this.front;
-      this.front = this.ground;
-      this.ground = this.back;
-      this.back = temp;
-    }
-    if (dir === 4) {
-      this.ceil = this.back;
-      this.back = this.ground;
-      this.ground = this.front;
-      this.front = temp;
+      this.gears[gear].clockDirectionRotate();
+    } else {
+      this.gears[gear].counterClockDirectionRotate();
     }
   }
 
-  paintGround(value) {
-    this.ground = value;
+  leftCheck(gear, dir) {
+    // 왼쪽 기어를 확인한다.
+    if (gear - 1 < 0) return;
+    if (this.visited[gear - 1]) return;
+    if (this.gears[gear - 1].rightValue() !== this.gears[gear].leftValue()) {
+      dir === -1
+        ? this.orderStack.push([gear - 1, 1])
+        : this.orderStack.push([gear - 1, -1]);
+    }
+  }
+  rightCheck(gear, dir) {
+    // 오른쪽 기어를 확인한다.
+    if (gear + 1 > 3) return;
+    if (this.visited[gear + 1]) return;
+    if (this.gears[gear].rightValue() !== this.gears[gear + 1].leftValue()) {
+      dir === -1
+        ? this.orderStack.push([gear + 1, 1])
+        : this.orderStack.push([gear + 1, -1]);
+    }
+  }
+
+  check(start, dir) {
+    this.orderStack.push([start, dir]);
+    while (this.orderStack.length > 0) {
+      const [gear, dir] = this.orderStack.shift();
+      this.visited[gear] = true;
+      this.leftCheck(gear, dir);
+      this.rightCheck(gear, dir);
+
+      this.workStack.push([gear, dir]);
+    }
+  }
+
+  do() {
+    while (this.workStack.length > 0) {
+      const [gear, dir] = this.workStack.shift();
+      this.work(gear, dir);
+    }
+    this.visited = [false, false, false, false];
+  }
+
+  countPoint() {
+    for (let i = 0; i < this.gears.length; i++) {
+      if (this.gears[i].topValue() === 1) {
+        this.point += 2 ** i;
+      }
+    }
+  }
+
+  print() {
+    console.log(this.gears);
   }
 }
 
-let cx = x;
-let cy = y;
-const dy = [0, 0, -1, 1];
-const dx = [1, -1, 0, 0];
+const gears = new Gears(inputs);
 
-const newDice = new Dice(0, 0, 0, 0, 0, 0);
-
-for (let i = 0; i < k; i++) {
-  const nx = cx + dx[orders[i] - 1];
-  const ny = cy + dy[orders[i] - 1];
-  if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-
-  newDice.rollDice(orders[i]);
-  if (map[ny][nx] === 0) {
-    map[ny][nx] = newDice.ground;
-  } else {
-    newDice.paintGround(map[ny][nx]);
-    map[ny][nx] = 0;
-  }
-
-  console.log(newDice.ceil);
-
-  cx = nx;
-  cy = ny;
+for (let i = 0; i < rotate.length; i++) {
+  gears.check(rotate[i][0] - 1, rotate[i][1]);
+  gears.do();
 }
+gears.countPoint();
+
+console.log(gears.point);
