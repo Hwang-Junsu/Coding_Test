5d0fe694845ef89e0bb2f876c417c29beb1af412
Hwang-Junsu <hjs77728@gmail.com>
2024-11-22 16:47:06 +0900

    24/11/22

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-const [first, second] = input;
+input.sort((a, b) => a.length - b.length);
+
+const [short, long] = input;
 
 // 이 반복문은 두 기어를 비교하는 반복문 하나는 움직이지 않고 가만히.
 
 function compare(ls, le, ss, se) {
+  console.log(ls, le, ss, se);
   for (let i = ls; i < le; i++) {
     for (let j = ss; j < se; j++) {
       if (+long[i] + +short[j] !== 3) return false;
@@ -23,27 +26,39 @@ function solution(l, s) {
   let le = 0;
   let ss = s.length;
   let se = s.length;
+  let min = l.length + s.length;
 
   while (true) {
+    if ((le === l.length) & (ss === 0) & (se > 1)) {
+      se--;
+    }
+    if ((le >= s.length) & (ls <= l.length - 2)) {
+      ls++;
+    }
     if (le < l.length) {
       le++;
     }
     if (ss > 0) {
       ss--;
     }
-    if (le > se && ls <= l.length - 2) {
-      ls++;
-    }
-    if (ls > se && se >= 1) {
-      se--;
+    // console.log(ls, le, ss, se);
+    if (compare(ls, le, ss, se)) {
+      min = Math.min(min, Math.abs(s.length - se - ss));
     }
 
-    console.log(ls, le, ss, se);
-    if (se === 1 && ss === 0 && le === l.length && ls === l.length - 1) break;
+    if (se === 1 && ss === 0 && le === l.length && ls === l.length - 1) {
+      break;
+    }
   }
+
+  return min;
 }
 
-solution(first, second);
+console.log(solution(long, short));
+
+// se - ss = 진입한 짧은 기어의 길이
+// ss = 아직 진입하지 않은 짧은기어의 길이
+// s.length - se - ss = 밖으로 나간 기어의 길이
 
 // 10 5 길이라고 가정
 // ls  le  ss  se
