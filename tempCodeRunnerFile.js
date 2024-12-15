b77e35f1919b9a31b4df53d46898792183b512bf
Hwang-Junsu <hjs77728@gmail.com>
2024-12-15 20:04:19 +0900

    24/12/15

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,3 @@
   // if (i === str.length - 1 && count > 0) {
-  //   if (activate) {
-  //     answer += 1;
-  //   } else {
-  //     answer += count > 1 ? 2 : 1;
-  //   }
+  //   answer += 1;
   // }
